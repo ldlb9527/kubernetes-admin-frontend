9f52e76e0041cf5ffde3ebf2637713ddb667645f
cx <1249843194@qq.com>
2022-07-27 00:51:10 +0800

    info

--- src/components/cluster/Info.vue
+++ src/components/cluster/Info.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="view-content">
-    <div id="myChart" :style="{width: '600px', height: '300px'}"></div>
+  <div>
+    <el-card class="node-status" >
+      <div class="view-content">
+        <div id="myChart" :style="{width: '600px', height: '300px'}"></div>
+      </div>
+    </el-card>
   </div>
+
 </template>
 
 <script>
@@ -24,55 +29,82 @@ export default {
       })
 
 
+      let value = 3;//当前进度
+      let maxValue = 4;//进度条最大值
       // 绘制图表配置
       let option = {
-        graphic: {
-          elements: [
+        title: {
+          //标题内容
+          text: '节点状态：',
+          textStyle: {
+            fontWeight: 'bold',
+            fontFamily: 'Microsoft YaHei',
+            fontSize: 24
+          },
+          //标题位置
+          top: '5%',
+          left: '5%'
+        },
+        //环形中间文字
+        graphic: [
+          //第一行文字
+          //内容 + 位置
+          {
+            type: 'text',
+            left: 'center',
+            top: '48%',
+            style: {
+              //value当前进度
+              text: value + '/' + maxValue,
+              textAlign: 'center',
+              fill: '#000',
+              fontSize: 28
+            }
+          }
+        ],
+        series: [{
+          type: 'pie',
+          radius: ['70%', '64%'],//['外圆大小', '内圆大小']
+          center: ['50%', '50%'],//圆心位置['左右'， '上下']
+          hoverAnimation: false,//取消鼠标悬停动画
+          animationEasing: 'cubicOut',//设置动画缓动效果
+          //取消显示饼图自带数据线条
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          //增加阴影效果
+          itemStyle: {
+            normal: {
+              shadowBlur: 200,
+              shadowColor: 'rgba(44, 196, 196, 0.8)'
+            }
+          },
+          data: [
+            //value当前进度 + 颜色
             {
-              type: 'text',
-              left: 'center',
-              top: 'center',
-              style: {
-                text: 'Kubernetes Admin',
-                fontSize: 40,
-                fontWeight: 'bold',
-                lineDash: [0, 200],
-                lineDashOffset: 0,
-                fill: 'transparent',
-                stroke: '#000',
-                lineWidth: 1
-              },
-              keyframeAnimation: {
-                duration: 10000,
-                loop: false,
-                keyframes: [
-                  {
-                    percent: 0.7,
-                    style: {
-                      fill: 'transparent',
-                      lineDashOffset: 200,
-                      lineDash: [200, 0]
-                    }
-                  },
-                  {
-                    // Stop for a while.
-                    percent: 0.8,
-                    style: {
-                      fill: 'transparent'
-                    }
-                  },
-                  {
-                    percent: 1,
-                    style: {
-                      fill: 'black'
-                    }
-                  }
-                ]
+              value: value,
+              itemStyle: {
+                normal: {
+                  color: '#73DEB3'
+                }
+              }
+            },
+            //(maxValue进度条最大值 - value当前进度) + 颜色
+            {
+              value: maxValue - value,
+              itemStyle: {
+                normal: {
+                  color: '#FF0000'
+                }
               }
             }
           ]
-        }
+        }]
       };
+
+
       // 窗口大小自适应方案
       myChart.setOption(option);
       setTimeout(function() {
@@ -86,5 +118,10 @@ export default {
 </script>
 
 <style scoped>
+.node-status {
+  width: 50%;
+
+
+}
 
 </style>
